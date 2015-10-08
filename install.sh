c5026531ac25a05517faff353f430caee8bb3fec
Gregory Petukhov <lorien@lorien.name>
2015-10-08 23:41:01 +0600

    fix

--- install.sh
+++ install.sh
@@ -139,8 +139,7 @@ aptitude install -y \
     openvpn iftop iotop \
     numactl unrar \
     python3.4 python3.4-dev python3-setuptools\
-    uwsgi uwsgi-plugin-python3 \
-    pigz nfs-common
+    pigz nfs-common curl
 
 if [ "$INSTALL_POSTGRES" == "YES" ]; then
     aptitude install -y \
@@ -191,217 +190,66 @@ echo "vim default editor"
 update-alternatives --set editor /usr/bin/vim.nox
 
 echo "python tools"
-# Python libraries
-easy_install -U pip
-easy_install -U distribute
-pip install -U virtualenv sr fabric pillow \
-    pymongo lxml pycurl grab argparse redis qr
 
-easy_install3 -U pip distribute
+# First install python3 things
+easy_install-3.4 -U pip distribute
 pip3 install -U virtualenv sr fabric pillow \
-    pymongo lxml pycurl grab argparse redis qr
+    pymongo lxml pycurl grab argparse redis qr gunicorn
 
-# Make default pip2
-pip2 install -I pip
+# Second install python2 things to make them be default things
+easy_install-2.7 -I pip
+pip2 install -I distribute virtualenv sr fabric pillow \
+    pymongo lxml pycurl grab argparse redis qr gunicorn
 
 if [ "$INSTALL_SUPERVISOR" == "YES" ]; then
-    pip install --upgrade --pre supervisor
+    pip install -U supervisor
     cat > /etc/supervisord.conf << EOF
 [unix_http_server]
-file=/var/run/supervisord.sock   ; (the path to the socket file)
+file=/var/run/supervisor.sock
+chmod=0770
+chown=root:web
 
-#[inet_http_server]         ; inet (TCP) server disabled by default
-#port=127.0.0.1:8888        ; (ip_address:port specifier, *:port for all iface)
-#username=***              ; (default is no username (open server))
-#password=***               ; (default is no password (open server))
+#[inet_http_server]
+#port=*:9000
+#username=***
+#password=***
 
 [supervisord]
-logfile=/var/log/supervisord.log ; (main log file;default $CWD/supervisord.log)
-logfile_maxbytes=50MB        ; (max main logfile bytes b4 rotation;default 50MB)
-logfile_backups=10           ; (num of main logfile rotation backups;default 10)
-loglevel=info                ; (log level;default info; others: debug,warn,trace)
-pidfile=/var/run/supervisord.pid ; (supervisord pidfile;default supervisord.pid)
-nodaemon=false               ; (start in foreground if true;default false)
-minfds=1024                  ; (min. avail startup file descriptors;default 1024)
-minprocs=200                 ; (min. avail process descriptors;default 200)
+logfile=/var/log/supervisord.log
+logfile_maxbytes=500MB
+logfile_backups=10
+loglevel=info
+pidfile=/var/run/supervisord.pid
+nodaemon=false
+minfds=1024
+minprocs=200
 
 [rpcinterface:supervisor]
 supervisor.rpcinterface_factory = supervisor.rpcinterface:make_main_rpcinterface
 
 [supervisorctl]
-serverurl=unix:///var/run/supervisord.sock ; use a unix:// URL  for a unix socket
+serverurl=unix:///var/run/supervisor.sock
 
-#[program:sphinx]
-#command=/usr/local/bin/searchd --nodetach
+#[include]
+#files = /web/site1/conf/supervisor.conf /web/site2/conf/supervisor.conf
 EOF
 
-    cat > /etc/inid.d/supervisord << EOF
-'#! /bin/sh
-### BEGIN INIT INFO
-# Provides:          supervisord
-# Required-Start:    $local_fs $remote_fs $networking
-# Required-Stop:     $local_fs $remote_fs $networking
-# Default-Start:     2 3 4 5
-# Default-Stop:      0 1 6
-# Short-Description: Starts supervisord - see http://supervisord.org
-# Description:       Starts and stops supervisord as needed - see http://supervisord.org
-### END INIT INFO
+    cat > /etc/systemd/system/supervisor.service << EOF
+[Unit]
+Description=Supervisor process control system for UNIX
+Documentation=http://supervisord.org
+After=network.target
 
-# Author: Leonard Norrgard <leonard.norrgard@refactor.fi>
-# Version 1.0-alpha
-# Based on the /etc/init.d/skeleton script in Debian.
+[Service]
+ExecStart=/usr/local/bin/supervisord -n -c /etc/supervisord.conf
+ExecStop=/usr/local/bin/supervisorctl $OPTIONS shutdown
+ExecReload=/usr/local/bin/supervisorctl $OPTIONS reload
+KillMode=process
+Restart=on-failure
+RestartSec=50s
 
-# Please note: This script is not yet well tested. What little testing
-# that actually was done was only on supervisor 2.2b1.
-
-# Do NOT "set -e"
-if [ -r /etc/default/supervisord ]; then
-    . /etc/default/supervisord
-fi
-    
-
-# PATH should only include /usr/* if it runs after the mountnfs.sh script
-PATH=/sbin:/usr/sbin:/bin:/usr/bin
-DESC="Run a set of applications as daemons."
-NAME=supervisord
-DAEMON=/usr/local/bin/$NAME   # Supervisord is installed in /usr/bin by default, but /usr/sbin would make more sense.
-SUPERVISORCTL=/usr/local/bin/supervisorctl
-PIDFILE=/var/run/$NAME.pid
-DAEMON_ARGS="--pidfile ${PIDFILE} -c /etc/supervisord.conf"
-SCRIPTNAME=/etc/init.d/$NAME
-
-# Exit if the package is not installed
-[ -x "$DAEMON" ] || exit 0
-
-# Read configuration variable file if it is present
-[ -r /etc/default/$NAME ] && . /etc/default/$NAME
-
-# Load the VERBOSE setting and other rcS variables
-. /lib/init/vars.sh
-
-# Define LSB log_* functions.
-# Depend on lsb-base (>= 3.0-6) to ensure that this file is present.
-. /lib/lsb/init-functions
-
-#
-# Function that starts the daemon/service
-#
-do_start()
-{
-	# Return
-	#   0 if daemon has been started
-	#   1 if daemon was already running
-	#   2 if daemon could not be started
-        [ -e $PIDFILE ] && return 1
-
-	WEB_USER=$WEB_USER start-stop-daemon --start --quiet --pidfile $PIDFILE --exec $DAEMON -- \
-		$DAEMON_ARGS \
-		|| return 2
-	# Add code here, if necessary, that waits for the process to be ready
-	# to handle requests from services started subsequently which depend
-	# on this one.  As a last resort, sleep for some time.
-}
-
-#
-# Function that stops the daemon/service
-#
-do_stop()
-{
-	# Return
-	#   0 if daemon has been stopped
-	#   1 if daemon was already stopped
-	#   2 if daemon could not be stopped
-	#   other if a failure occurred
-        [ -e $PIDFILE ] || return 1
-
-	# Stop all processes under supervisord control.
-	$SUPERVISORCTL stop all
-
-	start-stop-daemon --stop --quiet --retry=TERM/30/KILL/5 --pidfile $PIDFILE --name $NAME
-	RETVAL="$?"
-	[ "$RETVAL" = 2 ] && return 2
-	# Wait for children to finish too if this is a daemon that forks
-	# and if the daemon is only ever run from this initscript.
-	# If the above conditions are not satisfied then add some other code
-	# that waits for the process to drop all resources that could be
-	# needed by services started subsequently.  A last resort is to
-	# sleep for some time.
-	start-stop-daemon --stop --quiet --oknodo --retry=0/30/KILL/5 --exec $DAEMON
-	[ "$?" = 2 ] && return 2
-	# Many daemons don't delete their pidfiles when they exit.
-	rm -f $PIDFILE
-	return "$RETVAL"
-}
-
-#
-# Function that sends a SIGHUP to the daemon/service
-#
-do_reload() {
-	#
-	# If the daemon can reload its configuration without
-	# restarting (for example, when it is sent a SIGHUP),
-	# then implement that here.
-	#
-	start-stop-daemon --stop --signal 1 --quiet --pidfile $PIDFILE --name $NAME
-	return 0
-}
-
-case "$1" in
-  start)
-	[ "$VERBOSE" != no ] && log_daemon_msg "Starting $DESC" "$NAME"
-	do_start
-	case "$?" in
-		0|1) [ "$VERBOSE" != no ] && log_end_msg 0 ;;
-		2) [ "$VERBOSE" != no ] && log_end_msg 1 ;;
-	esac
-	;;
-  stop)
-	[ "$VERBOSE" != no ] && log_daemon_msg "Stopping $DESC" "$NAME"
-	do_stop
-	case "$?" in
-		0|1) [ "$VERBOSE" != no ] && log_end_msg 0 ;;
-		2) [ "$VERBOSE" != no ] && log_end_msg 1 ;;
-	esac
-	;;
-  #reload|force-reload)
-	#
-	# If do_reload() is not implemented then leave this commented out
-	# and leave 'force-reload' as an alias for 'restart'.
-	#
-	#log_daemon_msg "Reloading $DESC" "$NAME"
-	#do_reload
-	#log_end_msg $?
-	#;;
-  restart|force-reload)
-	#
-	# If the "reload" option is implemented then remove the
-	# 'force-reload' alias
-	#
-	log_daemon_msg "Restarting $DESC" "$NAME"
-	do_stop
-	case "$?" in
-	  0|1)
-		do_start
-		case "$?" in
-			0) log_end_msg 0 ;;
-			1) log_end_msg 1 ;; # Old process is still running
-			*) log_end_msg 1 ;; # Failed to start
-		esac
-		;;
-	  *)
-	  	# Failed to stop
-		log_end_msg 1
-		;;
-	esac
-	;;
-  *)
-	#echo "Usage: $SCRIPTNAME {start|stop|restart|reload|force-reload}" >&2
-	echo "Usage: $SCRIPTNAME {start|stop|restart|force-reload}" >&2
-	exit 3
-	;;
-esac
-
-:
+[Install]
+WantedBy=multi-user.target
 EOF
 fi
 
@@ -431,8 +279,8 @@ echo 'server {
     }   
 
     location / { 
-        include uwsgi_params;
-        uwsgi_pass unix:(WEB_DIR)/run/PROJECT.sock;
+        include proxy_params;
+        proxy_pass http://unix:(WEB_DIR)/run/PROJECT.sock:;
     }   
 }
 
@@ -451,8 +299,7 @@ echo 'alias ave="source .env/bin/activate"' >> /home/$WEB_USER/.bashrc
 mkdir /home/$WEB_USER/.ssh
 cp ~/.ssh/authorized_keys /home/$WEB_USER/.ssh
 chown -R web:web /home/$WEB_USER
-mkdir $WEB_DIR $WEB_DIR/run $WEB_DIR/log $WEB_DIR/etc \
-      $WEB_DIR/etc/uwsgi $WEB_DIR/etc/uwsgi/disabled
+mkdir $WEB_DIR $WEB_DIR/run $WEB_DIR/log $WEB_DIR/etc
 chown -R web:web $WEB_DIR
 cat > /home/$WEB_USER/.hgrc << 'EOF'
 [ui]
@@ -464,15 +311,13 @@ hgext.record=
 EOF
 chown web:web /home/$WEB_USER/.hgrc
 
-# Fix access to uwsgi directories
-chown root:$WEB_USER /etc/uwsgi/apps-enabled
-chown root:$WEB_USER /etc/uwsgi/apps-available
-chmod -R g+rwX /etc/uwsgi/apps-enabled
-chmod -R g+rwX /etc/uwsgi/apps-available
 
 if [ "$INSTALL_SUPERVISOR" == "YES" ]; then
-    addgroup supervisor
-    usermod -aG supervisor $WEB_USER
+    systemctl daemon-reload
+    systemctl enable supervisor
+    systemctl start supervisor
+    chown root:$WEB_USER /etc/supervisord.conf
+    chmod g+rw /etc/supervisord.conf
 fi
 
 if [ "$INSTALL_MYSQL" == "YES" ]; then
@@ -481,30 +326,9 @@ user=root
 password=' > /home/$WEB_USER/.my.cnf
 fi
 
-echo '[uwsgi]
-processes = 2
-socket = (WEB_DIR)/run/PROJECT.sock
-home = (WEB_DIR)/PROJECT/.env
-module = project.wsgi
-chmod-socket = 666
-master = true
-touch-reload = (WEB_DIR)/PROJECT/project/wsgi.py
-chdir = (WEB_DIR)/PROJECT
-uid = web
-gid = web
-reload-on-rss = 500
-limit-as = 500
-logto = (WEB_DIR)/log/PROJECT-uwsgi.log
-disable-logging = 1
-post-buffering=100000
-#plugin=python3' | sed 's!(WEB_DIR)!'$WEB_DIR'!g' > /etc/uwsgi/apps-available/template.ini
-
 echo "auto-start"
 # Настраиваем авто-запуск демонов, запускаем их
-update-rc.d uwsgi defaults
-update-rc.d nginx defaults
 /etc/init.d/nginx start
-/etc/init.d/uwsgi start
 
 if [ "$INSTALL_POSTGRES" == "YES" ]; then
     update-rc.d postgresql defaults
@@ -547,32 +371,6 @@ acl apache rep_header Server ^Apache
 broken_vary_encoding allow apache' > /etc/squid/squid.conf
 
 fi
-
-# echo "console-setup"
-# Отконфигурируем консоль
-# dpkg-reconfigure console-setup
-
-#echo "example-application"
-## Настроим и запустим демонстрационное приложение
-## Удалим дефолтный nginx сайт
-#unlink /etc/nginx/sites-enabled/default
-## Добавим nginx-конфиг для нового сайта
-#cat /etc/nginx/host.template | sed 's/PROJECT/test/g' | sed 's/HOST/test.local/g' > /etc/nginx/sites-enabled/test.local
-## Перезапустим nginx, чтобы он увидел новый конфиг
-#killall -HUP nginx
-## Создадим тестовое приложение
-#mkdir $WEB_DIR/test
-#cat > $WEB_DIR/test/app.py << EOF
-##!/usr/bin/env python
-## -*- coding: utf-8 -*-
-#def application(env, start_response):
-    #start_response('200 OK', [('Content-Type','text/html')])
-    #return "Application 1 responsed OK"
-#EOF
-#chown -R web:web $WEB_DIR/test
-## Создадим uwsgi-конфиг для нового сайта
-#cat $WEB_DIR/etc/uwsgi/disabled/template.ini | sed 's/PROJECT/test/g' > $WEB_DIR/etc/uwsgi/test.ini
-#chown web:web $WEB_DIR/etc/uwsgi/test.ini
 
 # Disable in-memory /tmp which was enabled by default in debian squeeze
 if [ -z "$(grep RAMTMP /etc/default/rcS)" ]; then
